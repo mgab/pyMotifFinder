5dc0ff5a9d25ae7f0b15aceb44f8da30c1bbc72c
mgab <mgab@users.noreply.github.com>
2016-05-26 18:10:59 +0200

    Create a __init__.py file so that it can be imported as a package.
    Also added a description of the aim of the module in the docstring
    of the same file.

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+u"""Module with the utilities to find network motifs in rather small networks.
+
+It is written for educative purposes using either trivial or already available
+algorithms. It has an emphasis on using an approach easy to understand, and
+thus it does not use more complex state-of-the-art algorithms.
+
+Author: Marçal Gabaldà <marcal.gabalda(at)upf(dot)edu>
+"""
+
+from __future__ import absolute_import
+
+from pyMotifFinder.ESU_find_motifs import *
